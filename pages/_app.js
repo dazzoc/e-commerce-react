b551579eabe0ff3c163b717218a8d3e58baf1e0c
Christopher Dazzo <dazzo.chris@gmail.com>
2022-06-16 14:34:05 -0500

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
+// import provider and createClient from urql
+import { Provider, createClient } from "urql";
+
+// create a client witch takes in our graphql URL
+const client = createClient({ url: "http://localhost:1337/graphql" })
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // wrap Provider around our components and pass in the client variable
+  return (
+    <Provider value={client}>
+    <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp
